3a08e2a5946d01ab937a820230afe05f7e9c6ba7
Austin Landis <austinlandiscode@gmail.com>
2021-01-25 17:13:48 -0800

    add function for form inputs.
    Co-authored-by: Sage Sypert <sage.sypert@gmail.com>
    Co-Authored by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -39,29 +39,37 @@ travelBook.addPlace(singapore);
 // User Interface Logic:
 
 $(document).ready(function() {
-  $("#Austin").click(function(){
-    console.log("clicked");
-    
-    $("#landis").text(`Austin has been to  ${travelBook.places[1].location},  where the landmark is a ${travelBook.places[1].landmark}. He went there in  ${travelBook.places[1].timeOfYear} and took these notes "${travelBook.places[1].notes}"! ${travelBook.places[1].traveler} is a great traveler.`);
-    $("#landis").toggle();
-  });
+  let addNewPlace;
+    $("#Austin").click(function(){
+      console.log("clicked");
+      
+      $("#landis").text(`Austin has been to  ${travelBook.places[1].location},  where the landmark is a ${travelBook.places[1].landmark}. He went there in  ${travelBook.places[1].timeOfYear} and took these notes "${travelBook.places[1].notes}"! ${travelBook.places[1].traveler} is a great traveler.`);
+      $("#landis").toggle();
+    });
 
-  $("#Sage").click(function(){
-    console.log("clicked");
-    $("#sypert").text(`Sage has been to ${travelBook.places[2].location}, where the landmark are the ${travelBook.places[2].landmark}. She went there in ${travelBook.places[2].timeOfYear} and took these notes "${travelBook.places[2].notes}"! ${travelBook.places[2].traveler} loves to travel.`);
-    $("#sypert").toggle();
-  });
+    $("#Sage").click(function(){
+      console.log("clicked");
+      $("#sypert").text(`Sage has been to ${travelBook.places[2].location}, where the landmark are the ${travelBook.places[2].landmark}. She went there in ${travelBook.places[2].timeOfYear} and took these notes "${travelBook.places[2].notes}"! ${travelBook.places[2].traveler} loves to travel.`);
+      $("#sypert").toggle();
+    });
 
-  $("#Olga").click(function(){
-    console.log("clicked");
-    $("#hizhytska").text(`I was live in ${travelBook.places[3].location } for 10 years, and I enjoyed a lot from a ${travelBook.places[3].landmark}. Last time I was there in ${travelBook.places[3].timeOfYear} and it was ${travelBook.places[3].notes}. It was written by ${travelBook.places[3].traveler}`);
-    $("#hizhytska").toggle();
-  });
+    $("#Olga").click(function(){
+      console.log("clicked");
+      $("#hizhytska").text(`I was live in ${travelBook.places[3].location } for 10 years, and I enjoyed a lot from a ${travelBook.places[3].landmark}. Last time I was there in ${travelBook.places[3].timeOfYear} and it was ${travelBook.places[3].notes}. It was written by ${travelBook.places[3].traveler}`);
+      $("#hizhytska").toggle();
+    });
 
-  $("#James").click(function(){
-    console.log("clicked");
+    $("#James").click(function(){
+      console.log("clicked");
 
-    $("#wyn").text(`${travelBook.places[4].traveler} has been to  ${travelBook.places[4].location},  where the landmark is ${travelBook.places[4].landmark}. He went there in  ${travelBook.places[4].timeOfYear} and took these notes "${travelBook.places[4].notes}"!`);
-    $("#wyn").toggle();
+      $("#wyn").text(`${travelBook.places[4].traveler} has been to  ${travelBook.places[4].location},  where the landmark is ${travelBook.places[4].landmark}. He went there in  ${travelBook.places[4].timeOfYear} and took these notes "${travelBook.places[4].notes}"!`);
+      $("#wyn").toggle();
+    });
+
+    $("form#places-form").submit(function(event) {
+      event.preventDefault();
+      addNewPlace = new Place($("input#add-location").val(), $("input#add-landmark").val(), $("input#add-toy").val(),$("input#add-notes").val(), $("input#add-traveler").val());
+      console.log(addNewPlace);
   });
 });
+
